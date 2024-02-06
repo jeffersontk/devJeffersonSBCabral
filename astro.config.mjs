d2136d13343d266a31e200007097c72443b1b0ee
jefferson <jscabral@firjan.com.br>
2024-02-06 00:50:40 -0300

    add ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
-import {sanityIntegration} from "@sanity/astro";
+import { sanityIntegration } from "@sanity/astro";
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  integrations: [
-    tailwind(), 
-    sanityIntegration({
+  output: 'server',
+  integrations: [tailwind(), sanityIntegration({
     projectId: "05a00f6m",
     dataset: "production",
     apiVersion: '2024-02-05',
     useCdn: false,
     // Access the Studio on your.url/admin
-    studioBasePath: "/admin",
-  }), 
-  react()
-]
+    studioBasePath: "/admin"
+  }), react()],
+  adapter: vercel()
 });
