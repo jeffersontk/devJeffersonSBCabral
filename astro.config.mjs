3be18c5f2dafa3b20f7e284669e8b6d68ecd3c24
jefferson <jscabral@firjan.com.br>
2024-02-07 00:44:40 -0300

    teste config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import { sanityIntegration } from "@sanity/astro";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   integrations: [tailwind(), sanityIntegration({
     projectId: "05a00f6m",
     dataset: "production",
