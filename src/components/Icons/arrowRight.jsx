eea62122727b83912c5d8b60ab8b8e4989224bd1
jefferson <jscabral@firjan.com.br>
2024-02-07 00:29:18 -0300

    change img to image astro

---
+++ src/components/Icons/arrowRight.jsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+const ArrowRight = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="#f8fafc"
+    viewBox="0 0 256 256"
+    {...props}
+  >
+    <path d="m181.66 133.66-80 80a8 8 0 0 1-11.32-11.32L164.69 128 90.34 53.66a8 8 0 0 1 11.32-11.32l80 80a8 8 0 0 1 0 11.32Z" />
+  </svg>
+)
+export default ArrowRight
